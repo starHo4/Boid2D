5d6bad62eabf2be5825a5517a17e2eb652476836
starHo4 <hi6yukiho4@gmail.com>
2021-08-22 14:37:33 +0900

    Remove test code.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
-// #include "GLScene.hpp"
+#include "GLScene.hpp"
 #include <iostream>
-#include <list>
 using namespace std;
 
 int main(int argc, char *argv[]){
-    // GLScene(argc, argv);
-    // glutMainLoop();
+    GLScene(argc, argv);
+    glutMainLoop();
 
     // Test //
-    list<char> lc;
-    for(int i=0; i<26; i++){
-        char ch = 'a' + i;
-        lc.push_back(ch);
-    }
-    list<char>::iterator itr;
-    for(itr = lc.begin(); itr != lc.end(); itr++){
-        cout << *itr << endl;
-    }
-    //
 }
